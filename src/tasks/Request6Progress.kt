123cde25f05d478510dd5f559941eb4df4f07b3d
xinlong_wu <v-xinwu@expediagroup.com>
2024-12-23 17:08:33 +0800

    request 6

--- src/tasks/Request6Progress.kt
+++ src/tasks/Request6Progress.kt
@@ -1,11 +1,25 @@
 package tasks
 
 import contributors.*
+import java.util.*
 
 suspend fun loadContributorsProgress(
     service: GitHubService,
     req: RequestData,
     updateResults: suspend (List<User>, completed: Boolean) -> Unit
 ) {
-    TODO()
+    val repos = service
+        .getOrgRepos(req.org)
+        .also { logRepos(req, it) }
+        .body() ?: emptyList()
+
+    val repoContributors = Collections.synchronizedList(mutableListOf<User>())
+    repos.forEach { repo ->
+        val contributors = service
+            .getRepoContributors(req.org, repo.name)
+            .also { logUsers(repo, it) }
+            .bodyList()
+        repoContributors += contributors
+        updateResults(repoContributors, repos.size == repoContributors.size)
+    }
 }
